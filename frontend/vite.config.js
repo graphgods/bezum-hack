c0221782c656f65d5cc5cadf3f6ee01c8d2d5186
prains <sleskov2013@yandex.ru>
2025-04-01 12:14:30 +0300

    add shadcn

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import { resolve } from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite({ autoCodeSplitting: true }), viteReact()],
+  plugins: [TanStackRouterVite({ autoCodeSplitting: true }), viteReact(), tailwindcss()],
   test: {
     globals: true,
     environment: "jsdom",
