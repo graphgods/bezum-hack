3e309a45df82ae8dfd7c45720be40e6cab06c30d
Timur Ermolaev <timur.e@indarchitects.com>
2025-04-01 14:02:43 +0300

    dobavil registraciu

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,6 +1,9 @@
-import { Outlet, createRootRoute } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
-import { Toaster } from 'sonner'
+import { Button } from "@/components/ui/button";
+import { Drawer, DrawerContent, DrawerTrigger } from "@/components/ui/drawer";
+import { Outlet, createRootRoute } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { Toaster } from "sonner";
+import kartaKartinka from '../images/photo_2025-04-01_13-58-15.jpg'
 
 export const Route = createRootRoute({
   component: () => (
@@ -8,6 +11,19 @@ export const Route = createRootRoute({
       <Outlet />
       <TanStackRouterDevtools />
       <Toaster />
+      <Drawer>
+        <DrawerTrigger asChild>
+          <Button className="text-xs size-20 p-0 block  rounded all break-all absolute right-4 bottom-4">
+            Вы
+            <br /> организатор <br />
+            БЕЗУМhack?
+          </Button>
+        </DrawerTrigger>
+        <DrawerContent className="text-center">
+          <h4 className="mt-20">Места где можно купить удлинитель</h4>
+          <img src={kartaKartinka} className="w-full h-100 mb-20"/>
+        </DrawerContent>
+      </Drawer>
     </>
   ),
-})
+});
