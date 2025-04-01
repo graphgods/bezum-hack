874dbf09d2a2eb6229175ed7c90c6436b78c4afd
ShIIIrochka <shiroooooo@yandex.ru>
2025-04-01 13:13:10 +0300

    ai

--- backend/src/utils/ai.py
+++ backend/src/utils/ai.py
@@ -1,26 +1,33 @@
+from dataclasses import dataclass
+
 from openai import OpenAI
 
 
-async def get_ai_answer(
-    api_key: str,
-    base_url: str,
-    model: str,
-    prompt: str,
-    message: str,
-) -> str:
-    client = OpenAI(
-        api_key=api_key,
-        base_url=base_url
-    )
-    response = client.chat.completions.create(
-        model=model,
-        messages=[
-            {
-                "role": "system", "content": prompt
-            },
-            {
-                "role": "user", "content": message
-            }
-        ]
-    )
-    return response.choices[0].message.content
+@dataclass(init=True)
+class AiClient:
+
+    model: str = "deepseek-ai/DeepSeek-R1-Distill-Llama-70B-free"
+    base_url: str = "https://api.together.xyz/v1"
+
+    async def get_answer(
+        self,
+        api_key: str,
+        prompt: str = "",
+        message: str = "",
+    ) -> str:
+        client = OpenAI(
+            api_key=api_key,
+            base_url=self.base_url
+        )
+        response = client.chat.completions.create(
+            model=self.model,
+            messages=[
+                {
+                    "role": "system", "content": prompt
+                },
+                {
+                    "role": "user", "content": message
+                }
+            ]
+        )
+        return response.choices[0].message.content
