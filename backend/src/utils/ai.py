2790148ed2ec27d0f1d46c7c043771424128e9fd
ShIIIrochka <shiroooooo@yandex.ru>
2025-04-01 14:32:25 +0300

    fsdfsdf

--- backend/src/utils/ai.py
+++ backend/src/utils/ai.py
@@ -6,14 +6,13 @@ from openai import OpenAI
 @dataclass(init=True)
 class AiClient:
 
-    model: str = "deepseek-ai/DeepSeek-R1-Distill-Llama-70B-free"
-    base_url: str = "https://api.together.xyz/v1"
+    model: str = "deepseek/deepseek-r1:free"
+    base_url: str = "https://openrouter.ai/api/v1"
 
     async def get_answer(
         self,
         api_key: str,
         prompt: str = "",
-        message: str = "",
     ) -> str:
         client = OpenAI(
             api_key=api_key,
@@ -25,9 +24,6 @@ class AiClient:
                 {
                     "role": "system", "content": prompt
                 },
-                {
-                    "role": "user", "content": message
-                }
             ]
         )
         return response.choices[0].message.content
